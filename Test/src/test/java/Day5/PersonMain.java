837b576e593a54629684a39a89bbd29075296b04
Sowmya <sowmyap.556@gmail.com>
2026-04-17 20:08:29 +0530

    Modify DOB

---
+++ Test/src/test/java/Day5/PersonMain.java
@@ -0,0 +1,14 @@
+package Day5;
+
+import java.time.LocalDate;
+
+public class PersonMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      ModifyPerson p = new ModifyPerson("Divya" , "Bharathi", 'F',LocalDate.of(1993, 3, 20));
+      p.display();
+      
+	}
+
+}
