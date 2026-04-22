fda3c6f1e6168e0b3779c3e2cd48dbe4492b8e77
Sowmya <sowmyap.556@gmail.com>
2026-04-22 12:06:08 +0530

    Account abstract

---
+++ Test/src/test/java/Day8/Account.java
@@ -0,0 +1,44 @@
+package Day8;
+
+import Day3.Person;
+
+abstract class Account {
+	
+		private int accNum;
+		protected double balance;
+		private Person accHolder;
+		
+		public Account(int accNum,Person accHolder, double balance)
+		{
+			this.accNum=accNum;
+			this.accHolder=accHolder;
+			this.balance=balance;
+		}
+		
+		public void deposit(double amount)
+		{
+			balance=balance+amount;
+		}
+		
+		public void withdraw(double amount)
+		{
+			if(balance-amount>=500)
+			{
+				balance=balance-amount;
+			}
+			else
+			{
+				System.out.println("Minimum balance 500 required");
+			}
+		}
+		
+		public double getBalance()
+		{
+			return balance;
+		}
+
+		public String toString()
+		{
+			return accNum+"  " + accHolder.getName()+" "+balance;
+		}
+}
